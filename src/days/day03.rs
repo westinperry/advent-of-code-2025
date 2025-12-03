09a82148c200d923c0674a8ba4858e1667b19397
westin <westinperry01@gmail.com>
2025-12-03 11:15:25 -0500

    Day 3 - Star 2

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -16,29 +16,43 @@ pub fn day_3() {
     let mut total_joltage = 0;
 
     for sequence in context_vec {
-        let chars: Vec<char> = sequence.chars().collect();
-        
-        let mut best_for_line = 0;
+        let digits: Vec<u32> = sequence.chars()
+            .map(|c| c.to_digit(10)
+            .expect("Invalid char"))
+            .collect();
 
-        for i in 0..chars.len() {
-            for j in i + 1..chars.len() {
-                let first = chars[i].to_digit(10).expect("invalid chars");
-                let second = chars[j].to_digit(10).expect("invalid chars");
+        let numbers_to_pick = 12;
+        let mut stack: Vec<u32> = Vec::new();
+        let mut numbers_to_remove = digits.len() - numbers_to_pick;
 
-                let value = 10 * first + second;
+        // Iterating over each digit
+        for &d in digits.iter() {
 
-                if value > best_for_line {
-                    best_for_line = value;
-                }
+            // While stack is not empty, last element smaller than current digit, and still elements to remove
+            while !stack.is_empty() && *stack.last().unwrap() < d && numbers_to_remove > 0 {
+                // Remove last stack element to attach larger digit
+                stack.pop();
+                numbers_to_remove -= 1;
             }
 
-            
+            // Push digit onto stack. (First digit or when needed to make 12 digits long or if 
+            // current digit is greater than current last stack element)
+            stack.push(d);
+
         }
-        //println!("Best line: {}", best_for_line);
-        total_joltage += best_for_line;
+
+        let mut final_number: u64 = 0;
+        // Iterate over first 12 numbers in stack
+        for &digit in stack.iter().take(numbers_to_pick) {
+            // Increase final number by factor 10, add current digit (till 12 digits)
+            final_number = final_number * 10 + digit as u64;
+        }
+
+        // Add to total
+        total_joltage += final_number;        
     }
 
-    println!("Voltage for day_3: {}", total_joltage)
+    println!("Total joltage for day 3: {}", total_joltage);
 
 }
 

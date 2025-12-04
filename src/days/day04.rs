8564221ee3a91a51296a64468059982ae373851f
westin <westinperry01@gmail.com>
2025-12-04 09:01:26 -0500

    Working grid creation from input file

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,6 +1,18 @@
-
+use std::fs;
 
 
 pub fn day_4() {
+    //let file_path = "inputs/day_4_input.txt";
+    let file_path = "inputs/day_4_input_test.txt";
+    let context = fs::read_to_string(file_path).expect("Error reading file");
 
+    // Creates a grid that of the input file.
+    // (collect chars of each line, second collect - collects all vectors)
+    let grid: Vec<Vec<char>> = context
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+
+
+    println!("{:?}", grid)
 }

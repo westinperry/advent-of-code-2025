74c0904da264303ed54b5a0c123dd8de252862a2
westin <westinperry01@gmail.com>
2025-12-08 14:53:08 -0500

    Day 6 Star 1

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,31 +1,30 @@
 use std::fs;
 
 pub fn day_6() {
-    //let file_path = "inputs/day_6_input.txt";
-    let file_path = "inputs/day_6_input_test.txt";
+    let file_path = "inputs/day_6_input.txt";
+    //let file_path = "inputs/day_6_input_test.txt";
     let context = fs::read_to_string(file_path).expect("Error reading file");
 
     let lines: Vec<&str> = context.lines().collect();
-    
-    
-    let number_grid: Vec<Vec<i32>> = lines[..lines.len()-1]
+
+    let number_grid: Vec<Vec<i64>> = lines[..lines.len() - 1]
         .iter()
         .map(|line| {
             line.split_whitespace()
-                .map(|n| n.parse::<i32>().unwrap())
+                .map(|n| n.parse::<i64>().unwrap())
                 .collect()
         })
         .collect();
 
-    let symb_row: Vec<char> = lines[lines.len()-1]
+    let symb_row: Vec<char> = lines[lines.len() - 1]
         .split_whitespace()
         .map(|s| s.chars().next().unwrap())
         .collect();
 
     let grid_height = number_grid.len();
     let grid_width = number_grid[0].len();
-    let mut result_row: Vec<i32> = Vec::new();
-    let mut result = 0;
+    let mut result_row: Vec<i64> = Vec::new();
+    let mut result: i64;
 
     for i in 0..grid_width {
         result = 0;
@@ -42,5 +41,5 @@ pub fn day_6() {
         result_row.push(result);
     }
 
-    println!("{:?}", result_row)
+    println!("{}", result_row.iter().sum::<i64>());
 }

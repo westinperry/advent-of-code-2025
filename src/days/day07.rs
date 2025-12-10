f118eaf084ab4beaddd5b1ae0a288b30b35a8a07
westin <westinperry01@gmail.com>
2025-12-10 15:32:02 -0500

    Day 7 second star, (needed some help with dfs implementation)

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -2,7 +2,7 @@ use std::{collections::HashMap, fs};
 
 pub fn day_7() {
     let file_path = "inputs/day_7_input.txt";
-    let file_path = "inputs/day_7_input_test.txt";
+    //let file_path = "inputs/day_7_input_test.txt";
     let context = fs::read_to_string(file_path).expect("Error reading file");
     let grid: Vec<Vec<char>> = context.lines().map(|line| line.chars().collect()).collect();
     let rows = grid.len();
@@ -53,4 +53,4 @@ pub fn day_7() {
     println!("{}", total_timelines)
 }
 
-
+// 27055852018812

6939d1140fe72c00800532731cc37ab5b4b49340
westin <westinperry01@gmail.com>
2025-12-11 15:12:13 -0500

    Added distance calc function

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,10 +1,38 @@
-use std::{collections::HashMap, fs};
+use std::{fs};
+
+struct Point {
+    x: f32,
+    y: f32,
+    z: f32
+}
 
 pub fn day_8() {
-    let file_path = "inputs/day_7_input.txt";
-    //let file_path = "inputs/day_7_input_test.txt";
+    //let file_path = "inputs/day_8_input.txt";
+    let file_path = "inputs/day_8_input_test.txt";
     let context = fs::read_to_string(file_path).expect("Error reading file");
-    let grid: Vec<Vec<char>> = context.lines().map(|line| line.chars().collect()).collect();
+    let points: Vec<Point> = context
+        .lines()
+        .map(|line| {
+            let coords: Vec<f32> = line
+                .split(',')
+                .map(|s| s.trim().parse().expect("Failed to parse number"))
+                .collect();
+            Point {
+                x: coords[0],
+                y: coords[1],
+                z: coords[2],
+            }
+        })
+        .collect();
+    let result = calc_3d_dist(&points[0], &points[1]);
 
+    println!("Distance: {:?}", result);
+}
+
+fn calc_3d_dist(p1: &Point, p2: &Point) -> f32 {
+
+    let dist = ((p2.x - p1.x).powf(2.0) + (p2.y - p1.y).powf(2.0) + (p2.z - p1.z).powf(2.0)).sqrt();
+
+    dist
 
 }

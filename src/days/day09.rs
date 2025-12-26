b30c918a825c69b9321eb989335006eec5c2b95e
westin <westinperry01@gmail.com>
2025-12-26 13:49:45 -0500

    Finished AoC

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,10 +1,10 @@
-use std::{fs};
+use std::fs;
 
 pub fn day_9() {
     let file_path = "inputs/day_9_input.txt";
 
     let input = fs::read_to_string(file_path).expect("Error reading file");
-    
+
     let parsed_cord_vec: Vec<Point> = input
         .lines()
         .filter(|line| !line.is_empty())
@@ -16,11 +16,11 @@ pub fn day_9() {
             }
         })
         .collect();
-    
-    let mut max:i64 = 0;
+
+    let mut max: i64 = 0;
 
     for i in 0..parsed_cord_vec.len() {
-        for j in i+1..parsed_cord_vec.len() {
+        for j in i + 1..parsed_cord_vec.len() {
             if is_valid_rect(&parsed_cord_vec[i], &parsed_cord_vec[j], &parsed_cord_vec) {
                 let result = calc_2d_dist(&parsed_cord_vec[i], &parsed_cord_vec[j]);
                 if result > max {
@@ -31,13 +31,12 @@ pub fn day_9() {
     }
 
     println!("Max area: {max}");
-
 }
 
 #[derive(Debug)]
 struct Point {
     x: i64,
-    y: i64
+    y: i64,
 }
 
 fn calc_2d_dist(p1: &Point, p2: &Point) -> i64 {
@@ -47,7 +46,6 @@ fn calc_2d_dist(p1: &Point, p2: &Point) -> i64 {
     let area = x * y;
 
     area
-
 }
 
 fn is_valid_rect(p1: &Point, p2: &Point, poly: &Vec<Point>) -> bool {
@@ -63,30 +61,36 @@ fn is_valid_rect(p1: &Point, p2: &Point, poly: &Vec<Point>) -> bool {
         let u = &poly[i];
         let v = &poly[(i + 1) % n];
 
-        if u.x == v.x { // Vertical Wall
+        if u.x == v.x {
+            // Vertical Wall
             let wall_x = u.x;
             let wall_min_y = u.y.min(v.y);
             let wall_max_y = u.y.max(v.y);
-            
+
             // If wall is strictly between X bounds and overlaps Y bounds
             if wall_x > min_x && wall_x < max_x {
-                if wall_max_y > min_y && wall_min_y < max_y { return false; }
+                if wall_max_y > min_y && wall_min_y < max_y {
+                    return false;
+                }
             }
-        } else { // Horizontal Wall
+        } else {
+            // Horizontal Wall
             let wall_y = u.y;
             let wall_min_x = u.x.min(v.x);
             let wall_max_x = u.x.max(v.x);
 
             // If wall is strictly between Y bounds and overlaps X bounds
             if wall_y > min_y && wall_y < max_y {
-                if wall_max_x > min_x && wall_min_x < max_x { return false; }
+                if wall_max_x > min_x && wall_min_x < max_x {
+                    return false;
+                }
             }
         }
     }
 
     // 2. Ray Casting: Check if the center is inside
     let center_x = (min_x + max_x) as f64 / 2.0;
-    let center_y = (min_y + max_y) as f64 / 2.0 + 0.01; 
+    let center_y = (min_y + max_y) as f64 / 2.0 + 0.01;
     let mut intersections = 0;
 
     for i in 0..n {
